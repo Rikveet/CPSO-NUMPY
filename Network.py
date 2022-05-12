Bug fixes to mse calculation.

Network.py
@@ -3,16 +3,23 @@ import numpy as np
 
 
 class Network:
-    def __init__(self, random, nodes_config, data):
+    def __init__(self, random, nodes_config, lda, data):
+        np.seterr(divide='ignore', invalid='ignore')
         self.random = random
+        self.lda = lda
         np.random.seed(10)
-        self.train_output_data = None
+        self.weight_decay = None
         self.train_input_data = None
+        self.train_output_data = None
+        self.test_input_data = None
+        self.test_output_data = None
+        self.batch_train_input_data = None
+        self.batch_train_output_data = None
         input_layer_nodes, hidden_layer_nodes, num_hidden_layers, output_layer_nodes = nodes_config
         self.out_layer = output_layer_nodes
         max_weight, min_weight = (-1, 1)
         self.data = data
-
+        self.load_all_data()
         # create weights and biases
         self.network_weights, self.network_biases = [], []
 
@@ -34,7 +41,7 @@ class Network:
         self.network_weights \
             .append(np.random.uniform(low=min_weight, high=max_weight, size=(hidden_layer_nodes, output_layer_nodes))
                     .astype('float32'))
-
+        self.update_weight_decay()
         np.set_printoptions(precision=2)
         print("Network initialized")
 
@@ -45,30 +52,84 @@ class Network:
         self.shuffle_data()
         input_data = []
         output_data = []
-        for i in range(100):
+        for i in range(100):  # 100
             inpt, outpt = self.data[i]
             input_data.append(inpt)
             output_data.append(outpt)
-        self.train_input_data = numpy.array(input_data).astype('float32')
-        self.train_output_data = numpy.array(output_data).astype('float32')
+        self.batch_train_input_data = numpy.array(input_data).astype('float32')
+        self.batch_train_output_data = numpy.array(output_data).astype('float32')
 
-    def get_weight_decay(self):
+    def load_all_data(self):
+        self.shuffle_data()
+        train_input_data = []
+        train_output_data = []
+        test_input_data = []
+        test_output_data = []
+        train_limit = int(len(self.data) * 0.80)
+        for i in range(train_limit):
+            Input, Output = self.data[i]
+            train_input_data.append(Input)
+            train_output_data.append(Output)
+        for i in range(train_limit, len(self.data)):
+            Input, Output = self.data[i]
+            test_input_data.append(Input)
+            test_output_data.append(Output)
+
+        self.train_input_data = numpy.array(train_input_data).astype('float32')
+        self.train_output_data = numpy.array(train_output_data).astype('float32')
+        self.test_input_data = numpy.array(test_input_data).astype('float32')
+        self.test_output_data = numpy.array(test_output_data).astype('float32')
+
+    def update_weight_decay(self):
         wd = 0
         for layer in self.network_weights:
             wd += np.sum(np.square(layer))
-        return wd
+        for bias_layer in self.network_biases:
+            wd += np.sum(np.square(bias_layer))
+        self.weight_decay = (wd / (wd + 1)) * self.lda
 
-    def feed_forward(self, show=False):
-        value = 0
-        value_layer = self.train_input_data
-        for i in range(len(self.network_weights)):
+    def feed_forward(self):
+        value_layer = self.batch_train_input_data
+        for i in range(len(self.network_weights)):  # batch
             value_layer = np.maximum(np.dot(value_layer, self.network_weights[i]) + self.network_biases[i], 0)
-        value += np.sum(np.square(value_layer - self.train_output_data))
+        avg = (np.sum(value_layer, axis=1)).reshape((len(value_layer), 1))
+        avg[avg == 0] = 0.001
+        value_layer = value_layer / avg
+        value = np.sum(np.square(value_layer - self.batch_train_output_data))
+        fitness = value / (self.out_layer * len(self.batch_train_input_data))
+        return fitness + self.weight_decay
+
+    def feed_forward_global(self, show=False):
+        value_layer = self.train_input_data
+        for i in range(len(self.network_weights)):  # whole
+            value_layer = np.maximum(np.dot(value_layer, self.network_weights[i]) + self.network_biases[i], 0)
+        avg = (np.sum(value_layer, axis=1)).reshape((len(value_layer), 1))
+        avg[avg == 0] = 0.001
+        value_layer = value_layer / avg
+        value = np.sum(np.square(value_layer - self.train_output_data))
+        global_train_fitness = value / (self.out_layer * len(self.train_input_data))
         if show:
-            for i in range(100):
+            value_layer = np.round(value_layer, 2)
+            for i in range(len(self.train_input_data)):
                 print("{}..".format(str(self.train_input_data[i])[:4]), "::", self.train_output_data[i], "::",
                       value_layer[i])  # )
-        return value / (self.out_layer * 100)
+        return global_train_fitness + self.weight_decay
+
+    def feed_forward_global_test(self, show=False):
+        value_layer = self.test_input_data
+        for i in range(len(self.network_weights)):  # whole
+            value_layer = np.maximum(np.dot(value_layer, self.network_weights[i]) + self.network_biases[i], 0)
+        avg = (np.sum(value_layer, axis=1)).reshape((len(value_layer), 1))
+        avg[avg == 0] = 0.001
+        value_layer = value_layer / avg
+        value = np.sum(np.square(value_layer - self.test_output_data))
+        global_test_fitness = value / (self.out_layer * len(self.test_input_data))
+        if show:
+            value_layer = np.round(value_layer, 2)
+            for i in range(len(self.test_input_data)):
+                print("{}..".format(str(self.test_input_data[i])[:4]), "::", self.test_output_data[i], "::",
+                      value_layer[i])
+        return global_test_fitness + self.weight_decay
 
     def update_weights(self, params, value):
         layer, node, out_node = params
@@ -76,6 +137,7 @@ class Network:
             self.network_biases[layer][0, node] = value
         else:
             self.network_weights[layer][node, out_node] = value
+        self.update_weight_decay()
 
     def test(self, params, value):
         layer, node, out_node = params
@@ -88,5 +150,19 @@ class Network:
             current_value = self.network_weights[layer][node, out_node]
             self.network_weights[layer][node, out_node] = value
             test_value = self.feed_forward()
+            self.network_weights[layer][node, out_node] = current_value
+        return test_value
+
+    def globalTest(self, params, value):
+        layer, node, out_node = params
+        if out_node == -1:
+            current_value = self.network_biases[layer][0, node]
+            self.network_biases[layer][0, node] = value
+            test_value = self.feed_forward_global()
+            self.network_biases[layer][0, node] = current_value
+        else:
+            current_value = self.network_weights[layer][node, out_node]
+            self.network_weights[layer][node, out_node] = value
+            test_value = self.feed_forward_global()
             self.network_weights[layer][node, out_node] = current_value
         return test_value

main.py
@@ -1,4 +1,5 @@
 import copy
+import csv
 import math
 from random import Random as Rand
 
@@ -11,60 +12,125 @@ from Network import Network
 
 class Cpso:
 
-    def __init__(self):
-        self.random = Rand()
-        self.random.seed(10)
-        particles = 20
-        network_config, self.data = data_loader.get_breast_cancer_data()
-        self.input_nodes = network_config[0]
-        self.hidden_nodes = network_config[1]
-        self.output_nodes = network_config[3]
-        self.contextVector = self.generateOneVec(network_config)
-        self.dimension = len(self.contextVector)
-        # pprint(self.contextVector)
-        self.layers = 2 + network_config[2]  # input layer + hidden layers + output layer
-        # decomposition = self.psoDecompose()
-        # decomposition = self.layerDecompose()
-        # decomposition = self.factorizedLayerDecompose()
-        # decomposition = self.nodeDecompose()
-        decomposition = self.factorizedNodeDecompose()
-        self.swarms = []
-        self.swarms = self.createSwarms(decomposition)
-        self.net = Network(self.random, network_config, self.data)
-        self.iterations = 200
-        self.merge_nr = 2
-        self.do_merge = False
-        self.merge_iter = math.floor(self.iterations / (1 + (math.log(self.dimension) / math.log(self.merge_nr))))
-
-        decomposition_type = "node_factorized"
-        self.decom_func = None
-        decomDict = {
-            "layer": {
-                "function": self.layerDecompose,
-                "sub_swarm_size": self.layers - 1
-            },
-            "layer_factorized": {
-                "function": self.factorizedLayerDecompose,
-                "sub_swarm_size": self.output_nodes + 1
-            },
-            "node": {
-                "function": self.nodeDecompose,
-                "sub_swarm_size": self.hidden_nodes + self.output_nodes
-            },
-            "node_factorized": {
-                "function": self.factorizedNodeDecompose,
-                "sub_swarm_size": self.hidden_nodes + self.output_nodes
+    def __init__(self, variant_decompose_parameters):
+        seeds = [10402, 10418, 10598, 10859, 11177, 11447, 12129, 12497, 13213, 13431, 13815, 14573, 15010, 15095,
+                 15259, 16148, 17020, 17172, 17265, 17291, 17307, 17591, 17987, 18284, 18700, 18906, 19406, 19457,
+                 19482, 19894]
+        for parameter in variant_decompose_parameters:
+            key = ""
+            decomposition_type = None
+            if "decomposition" in parameter.keys():
+                key = parameter["variant"] + "_" + parameter["decomposition"] + "_" + parameter["data_set"]
+                decomposition_type = parameter["decomposition"]
+            else:
+                key = parameter["variant"] + "_" + parameter["data_set"]
+            self.result = {
+                "iterations": [],
+                "train": [],
+                "test": []
             }
-        }
-        self.do_decompose = False
-        if self.do_decompose and (decomposition_type in decomDict.keys()):
-            self.decom_func = decomDict[decomposition_type]["function"]
-            self.decompose_iter = math.floor(
-                self.iterations /
-                (1 + (math.log(self.dimension) / math.log(decomDict[decomposition_type]["sub_swarm_size"]))))
-        else:
-            self.do_decompose = False
-        self.optimize()
+            data_dict = {
+                "iris": data_loader.get_iris_data,
+                "wine": data_loader.get_wine_data,
+                "cancer": data_loader.get_breast_cancer_data
+            }
+            if parameter["data_set"] is not None and parameter["data_set"] in data_dict.keys():
+                network_config, self.data = data_dict[parameter["data_set"]]()
+                for seed in seeds:
+                    self.random = Rand()
+                    self.random.seed(seed)
+                    lda = 0.001  # lambda
+                    self.input_nodes = network_config[0]
+                    self.hidden_nodes = network_config[1]
+                    self.output_nodes = network_config[3]
+                    self.contextVector = self.generateOneVec(network_config)
+                    self.dimension = len(self.contextVector)
+                    self.layers = 2 + network_config[2]  # input layer + hidden layers + output layer
+                    decomposition = None
+                    self.decom_func = False
+                    self.do_decompose = False
+                    self.do_merge = False
+                    self.iterations = parameter["iterations"]
+                    decomDict = {
+                        "layer": {
+                            "function": self.layerDecompose,
+                            "sub_swarm_size": self.layers - 1
+                        },
+                        "layer_factorized": {
+                            "function": self.factorizedLayerDecompose,
+                            "sub_swarm_size": self.output_nodes + 1
+                        },
+                        "node": {
+                            "function": self.nodeDecompose,
+                            "sub_swarm_size": self.hidden_nodes + self.output_nodes
+                        },
+                        "node_factorized": {
+                            "function": self.factorizedNodeDecompose,
+                            "sub_swarm_size": self.hidden_nodes + self.output_nodes
+                        }
+                    }
+                    if parameter["variant"] == "pso":
+                        decomposition = self.psoDecompose()
+                    else:
+                        if decomposition_type is not None and decomposition_type in decomDict.keys():
+                            if parameter["variant"] == "dcpso":
+                                decomposition = self.psoDecompose()
+                                self.do_decompose = True
+                                self.decom_func = decomDict[decomposition_type]["function"]
+                                self.decompose_iter = math.floor(
+                                    self.iterations /
+                                    (1 + (math.log(self.dimension) / math.log(
+                                        decomDict[decomposition_type]["sub_swarm_size"]))))
+                            elif parameter["variant"] == "cpso":
+                                decomposition = decomDict[decomposition_type]["function"]()
+                            elif parameter["variant"] == "mcpso":
+                                decomposition = decomDict[decomposition_type]["function"]()
+                                self.do_merge = True
+                                self.merge_nr = 2
+                                self.merge_iter = math.floor(
+                                    self.iterations / (1 + (math.log(self.dimension) / math.log(self.merge_nr))))
+                            else:
+                                print("Invalid variant: ", parameter["variant"],
+                                      " Available variants: pso, dcpso, cpso, mcpso(case sensitive)")
+                        else:
+                            print("Invalid decomposition ", decomposition_type, " Available options are: ",
+                                  decomDict.keys())
+                    if decomposition is not None:
+                        self.swarms = []
+                        self.swarms = self.createSwarms(decomposition)
+                        self.net = Network(self.random, network_config, lda, self.data)
+                        self.optimize()
+                    else:
+                        print("Invalid Decomposition: ", parameter["decomposition"])
+                avg_result = {
+                    "iterations": np.average(np.array(self.result["iterations"]), axis=0),
+                    "train": np.average(np.array(self.result["train"]), axis=0),
+                    "test": np.average(np.array(self.result["test"]), axis=0)
+                }
+                interation_plot = np.arange(0, parameter["iterations"] + 1, 1)
+                plt.clf()
+                plt.plot(interation_plot, avg_result["iterations"])
+                plt.title("Mse over training set for " + parameter["data_set"] + " data set with " +
+                          parameter["variant"] + " variant.")
+                plt.xlabel("Iterations")
+                plt.ylabel("Training Mse")
+                plt.savefig("./Results/" + key, dpi=300)
+                plt.show()
+                with open("./Results/" + key + ".csv", "w", newline="") as f:
+                    writer = csv.writer(f)
+                    writer.writerow(list(interation_plot))
+                    writer.writerows(self.result["iterations"])
+                    writer.writerow(avg_result["iterations"].tolist())
+                    writer.writerow(["train"])
+                    writer.writerow(self.result["train"])
+                    writer.writerow(["average train"])
+                    writer.writerow([avg_result["train"].tolist()])
+                    writer.writerow(["test"])
+                    writer.writerow(self.result["test"])
+                    writer.writerow(["average test"])
+                    writer.writerow([avg_result["test"].tolist()])
+            else:
+                print("Invalid data set named: ", parameter["data_set"], " Available data-sets are ", data_dict.keys())
 
     def createWeight(self, index: int, layer: int, node: int, j: int, max_particle_value: float,
                      min_particle_value: float) -> {}:
@@ -257,6 +323,7 @@ class Cpso:
         return swarms
 
     def merge(self):
+        prev_num_swarms = len(self.swarms)
         if len(self.swarms) > 1:
             swarms = []
             for i in range(0, len(self.swarms), 2):
@@ -292,8 +359,9 @@ class Cpso:
                         swarms[i]["particles"][j][p]["f"] = math.inf
                         swarms[i]["particles"][j][p]["v"] = 0
             self.swarms = swarms
-
-        print("Current swarms :", len(self.swarms))
+        else:
+            self.do_merge = False
+        print("Merge swarms : ", prev_num_swarms, " -> ", len(self.swarms))
 
     def decompose(self):
         decomposition = self.decom_func()
@@ -304,58 +372,149 @@ class Cpso:
                 swarm["values"][value] = copy.deepcopy(values[value])
         self.swarms = swarms
         self.do_decompose = False
+        print("Decompose swarms : ", 1, " -> ", len(self.swarms))
 
     def optimize(self):
         max_velocity = 0.14286
         c1 = 1.49618
         c2 = 1.49618
         w = 0.729844
-        lda = 0.001  # lambda
-        y_values = []
+        y_values = [self.net.feed_forward_global()]
         iterations = self.iterations
         for iteration in range(iterations):
-            self.net.create_batch()
             if self.do_merge and iteration % self.merge_iter == 0 and iteration != 0:
+                self.random.shuffle(self.swarms)
                 self.merge()
             elif self.do_decompose and iteration % self.decompose_iter == 0 and iteration != 0:
+                self.random.shuffle(self.swarms)
                 self.decompose()
             for subSwarm in self.swarms:
-                # update best local values and personal values
                 for i in range(20):
                     for particle in subSwarm["particles"]:
                         node_params = (particle[i]["layer"], particle[i]["node"], particle[i]["j"])
-                        fitness = self.net.test(node_params, particle[i]["w"])
-                        if fitness <= particle[i]["f"]:
-                            particle[i]["bw"] = particle[i]["w"]
-                            particle[i]["f"] = fitness
-                        if fitness <= subSwarm["values"][particle[i]["key"]]["f"]:
-                            subSwarm["values"][particle[i]["key"]]["lbw"] = particle[i]["w"]
-                            subSwarm["values"][particle[i]["key"]]["f"] = fitness
-                            self.net.update_weights(node_params, particle[i]["w"])
+                        global_fitness = self.net.globalTest(node_params, particle[i]["bw"])
+                        if global_fitness <= subSwarm["values"][particle[i]["key"]]["f"]:
+                            subSwarm["values"][particle[i]["key"]]["lbw"] = particle[i]["bw"]
+                            subSwarm["values"][particle[i]["key"]]["f"] = global_fitness
+                            self.net.update_weights(node_params, particle[i]["bw"])
+            for subSwarm in self.swarms:
                 # update velocity and position
                 for i in range(20):
                     for particle in subSwarm["particles"]:
-                        w_sum = self.net.get_weight_decay()
-                        wd = lda * (w_sum / (w_sum + 1))
+                        self.net.create_batch()
                         v = (w * particle[i]["v"]) + (
                                 c1 * self.random.uniform(0, 1) * (particle[i]["bw"] - particle[i]["w"])) + \
                             (c2 * self.random.uniform(0, 1) * (
                                     subSwarm["values"][particle[i]["key"]]["lbw"] - particle[i]["w"]))
                         v = min(max(-max_velocity, v), max_velocity)
                         particle[i]["v"] = v
-                        particle[i]["w"] += v + wd
+                        particle[i]["w"] += v
                         particle[i]["w"] = min(max(-1, particle[i]["w"]), 1)
-            fitness = self.net.feed_forward()
-            print("Iteration", iteration, " Fitness: ", fitness)
-            y_values.append(fitness)
+                        node_params = (particle[i]["layer"], particle[i]["node"], particle[i]["j"])
+                        particle[i]["f"] = self.net.test(node_params,
+                                                         particle[i]["bw"])  # best personal position fitness
+                        fitness = self.net.test(node_params, particle[i]["w"])  # current position fitness
+                        if fitness < particle[i]["f"]:  # better position
+                            particle[i]["bw"] = particle[i]["w"]
+                            particle[i]["f"] = fitness
+
+            global_fitness = self.net.feed_forward_global()
+            print("Iteration", iteration, " Fitness: ", global_fitness)
+            y_values.append(global_fitness)
         print("Training Complete, output:->")
-        plt.axis([0, iterations, 0, max(y_values)])
-        plt.plot(np.array(y_values))
-        plt.show()
-        final_fitness = self.net.feed_forward(True)
-        print("Final fitness:", final_fitness)
+        global_fitness = self.net.feed_forward_global(True)
+        print("Testing output:->")
+        global_test_fitness = self.net.feed_forward_global_test(True)
+        print("Final train fitness:", global_fitness)
+        print("Final test fitness:", global_test_fitness)
+        self.result["iterations"].append(y_values)
+        self.result["train"].append(global_fitness)
+        self.result["test"].append(global_test_fitness)
 
 
 if __name__ == "__main__":
     np.set_printoptions(suppress=True)
-    Cpso()
+    iters = 200
+    data_sets = ["iris", "wine", "cancer"]
+    for data_set in data_sets:
+        parameters = [
+            {
+                "variant": "pso",
+                "iterations": iters,
+                "data_set": data_set
+            },
+            {
+                "variant": "dcpso",
+                "decomposition": "node",
+                "iterations": iters,
+                "data_set": data_set
+            },
+            {
+                "variant": "dcpso",
+                "decomposition": "node_factorized",
+                "iterations": iters,
+                "data_set": data_set
+            },
+            {
+                "variant": "dcpso",
+                "decomposition": "layer",
+                "iterations": iters,
+                "data_set": data_set
+            },
+            {
+                "variant": "dcpso",
+                "decomposition": "layer_factorized",
+                "iterations": iters,
+                "data_set": data_set
+            }
+            ,
+            {
+                "variant": "mcpso",
+                "decomposition": "node",
+                "iterations": iters,
+                "data_set": data_set
+            },
+            {
+                "variant": "mcpso",
+                "decomposition": "node_factorized",
+                "iterations": iters,
+                "data_set": data_set
+            },
+            {
+                "variant": "mcpso",
+                "decomposition": "layer",
+                "iterations": iters,
+                "data_set": data_set
+            },
+            {
+                "variant": "mcpso",
+                "decomposition": "layer_factorized",
+                "iterations": iters,
+                "data_set": data_set
+            },
+            {
+                "variant": "cpso",
+                "decomposition": "node",
+                "iterations": iters,
+                "data_set": data_set
+            },
+            {
+                "variant": "cpso",
+                "decomposition": "node_factorized",
+                "iterations": iters,
+                "data_set": data_set
+            },
+            {
+                "variant": "cpso",
+                "decomposition": "layer",
+                "iterations": iters,
+                "data_set": data_set
+            },
+            {
+                "variant": "cpso",
+                "decomposition": "layer_factorized",
+                "iterations": iters,
+                "data_set": data_set
+            }
+        ]
+        Cpso(parameters)