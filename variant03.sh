077bf8d5793c7551e940732824e1cf1ee58a391b
CaptKillSwitch <62815232+CaptKillSwitch@users.noreply.github.com>
2022-06-12 21:44:45 -0400

    udpated variant test sh

--- variant03.sh
+++ variant03.sh
@@ -1,1 +1,5 @@
-python3 main.py $dataset cpso layer_factorized
+#!/bin/bash
+# The following line can be included if you have multi-thread programs make sure the #PBS stays at the front of the line
+# PBS -l select=1:ncpus=2:mem=8gb
+
+/usr/bin/scl enable rh-python38 'python3 main.py $dataset cpso layer_factorized'
