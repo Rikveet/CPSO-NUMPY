5ccd49f723ec7f17b7ca9d4353f3d57bd1ba5861
CaptKillSwitch <62815232+CaptKillSwitch@users.noreply.github.com>
2022-06-12 21:51:49 -0400

    fixed dataset variable in command for variant sh

--- variant08.sh
+++ variant08.sh
@@ -2,4 +2,4 @@
 # The following line can be included if you have multi-thread programs make sure the #PBS stays at the front of the line
 # PBS -l select=1:ncpus=2:mem=8gb
 
-/usr/bin/scl enable rh-python38 'python3 main.py $dataset dcpso node'
+/usr/bin/scl enable rh-python38 'python3 main.py '$dataset' dcpso node'
