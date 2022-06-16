95e7013e8265527eeeb1fb19fe74f094f6bfc7a1
CaptKillSwitch <62815232+CaptKillSwitch@users.noreply.github.com>
2022-06-16 15:13:49 -0400

    slight change to vsb command

--- variant01.sh
+++ variant01.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # The following line can be included if you have multi-thread programs make sure the #PBS stays at the front of the line
-# PBS -l select=1:ncpus=2:mem=8gb
+#PBS -l select=1:ncpus=2:mem=7gb
 dataset=$1
 
 /usr/bin/scl enable rh-python38 'python3 ./experiment/CPSO-NUMPY/main.py '$dataset' pso'
